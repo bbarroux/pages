1c876a12f9675676edf50217033252d454c2514d
Barroux, Bertrand <Bertrand.Barroux@misys.com>
2020-08-30 19:01:25 +0200

    publish

--- publish.bat
+++ publish.bat
@@ -1,3 +1,4 @@
 cd %~dp0
 git commit -am "publish" && git push
 aws s3 sync . s3://sso-demo/ --exclude="*" --include="*.html"
+az storage blob sync -s . -c fficaas --account-name p21d11107027001 --include-pattern "*.html"
