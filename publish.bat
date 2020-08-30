5f699915bfd7e8673440b269b882f744d741ca74
Barroux, Bertrand <Bertrand.Barroux@misys.com>
2020-08-30 18:29:15 +0200

    publish

--- publish.bat
+++ publish.bat
@@ -1,3 +1,3 @@
 cd %~dp0
 git commit -am "publish" && git push
-aws s3 sync . s3://sso-demo/ --recursive --exclude="*" --include="*.html"
+aws s3 sync . s3://sso-demo/ --exclude="*" --include="*.html"
